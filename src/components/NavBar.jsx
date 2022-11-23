62b5589444f566e2b9d6b8759548a1f9823a2510
makaempffer <ma.kaempffer@duocuc.cl>
2022-11-23 20:33:16 -0300

    Added project cards w/fecth API

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,38 +3,38 @@ export default function NavBar() {
     return(
         <div className="w-screen flex h-20 bg-orangeMenu items-center justify-center space-x-20 font-outfit ">
             <div className="relative group">
-                <button className="text-white font-bold tracking-wider">Projects</button>
+                <button className="text-white font-bold tracking-wider hover:scale-105 duration-500">Projects</button>
                 <div className="invisible group-hover:visible">
                     <ul className="absolute text-white pt-7">
                         <li>
-                            <a className="bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap" href="/projects">New Project</a>
+                            <a className="bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap duration-300" href="/projects">New Project</a>
                         </li>
                         <li>
-                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap" href="#none">Recent</a>
+                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap duration-300" href="#none">Recent</a>
                         </li>
                         <li>
-                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap rounded-b" href="#none">All Projects</a>
+                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap rounded-b duration-300" href="#none">All Projects</a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div className="relative group">
-                <button className="text-white font-bold tracking-wider">Teams</button>
+                <button className="text-white font-bold tracking-wider hover:scale-105 duration-500">Teams</button>
                 <div className="invisible group-hover:visible">
                     <ul className="absolute text-white pt-7">
                         <li>
-                            <a className="bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap" href="#none">New Team</a>
+                            <a className="bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap duration-300" href="#none">New Team</a>
                         </li>
                         <li>
-                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap" href="#none">Recent</a>
+                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap duration-300" href="#none">Recent</a>
                         </li>
                         <li>
-                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap rounded-b" href="#none">All Teams</a>
+                            <a className=" bg-orangeMenu hover:bg-orange-200 py-2 px-4 block whitespace-nowrap rounded-b duration-300" href="#none">All Teams</a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <button className="text-white font-bold tracking-wider">Calendar</button>
+            <button className="text-white font-bold tracking-wider hover:scale-105 duration-500">Calendar</button>
         </div>
     )
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react";
 
 
 export default function Project() {
     return(
-        <div className="block w-40 h-40 p-5 rounded-br bg-grayCustom">
-            <button className="rounded">Add Task</button>
-            <button className="rounded">Add Task</button>
-            <button className="rounded">Add Task</button>
-            <button className="rounded">Add Task</button>
+        <div>
+            <div className="flex w-full h-10 justify-evenly rounded-br bg-grayCustom">
+                <button className="rounded font-roboto">Add Task Panel</button>
+                <button className="rounded font-roboto">2</button>
+                <button className="rounded font-roboto">3</button>
+                <button className="rounded font-roboto">4</button>
+            </div>
+            <div className="flex space-x-5 p-5">
+                <MyComponent />
+            </div>
         </div>
+    )
+}
+
+
+function MyComponent() {
+    const [panels, setPanels] = useState([]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:8090/api/collections/panels/records?page=1&perPage=30')
+        .then(response => response.json())
+        .then(data => {
+            setPanels(data.items)
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    }, []);
+    return (
+        panels.map((panel) =>(
+            <div className="w-1/4 h-32 bg-black rounded p-3">
+                <p className="text-white" key={panel.id}>{panel.title}</p>
+            </div>
+        ))
+        // <div className="w-1/4 h-32 bg-black rounded p-3">
+        //     {panels.map((panel) => (
+        //         <p className="text-white" key={panel.id}>{panel.title}</p>
+        //     ))}
+        // </div>
     )
 }
